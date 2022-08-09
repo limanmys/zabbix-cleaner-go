357bcefe8aeaaa10a7944fe9f8390104a6b40d26
Zeki Ahmet Bayar <54356667+zekiahmetbayar@users.noreply.github.com>
2022-08-09 16:22:05 +0300

    Update logger.go

--- pkg/utils/logger.go
+++ pkg/utils/logger.go
@@ -22,7 +22,7 @@ func InitLogger() {
 		log.Fatal("cannot create log directories")
 	}
 
-	directories := []string{"alerts", "events", "history", "history_uint", "history_str", "history_text", "auditlog", "acknowledges", "trends", "trends_uint"}
+	directories := []string{"events", "history", "history_uint", "history_str", "history_text", "auditlog", "acknowledges", "trends", "trends_uint"}
 	for _, element := range directories {
 		err = os.MkdirAll(filepath.FromSlash("/var/log/zabbix-db-cleaner/deleted_rows/"+element+"/"), 0770)
 		if err != nil {
@@ -30,7 +30,7 @@ func InitLogger() {
 		}
 	}
 
-	info, err := os.OpenFile(filepath.FromSlash("/var/log/zabbix-cleaner/cleaner.log"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0770)
+	info, err := os.OpenFile(filepath.FromSlash("/var/log/zabbix-db-cleaner/cleaner.log"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0770)
 	if err != nil {
 		log.Fatal("cannot create log file")
 	}
